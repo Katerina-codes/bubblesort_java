92cba5502d1498008d4418f9ea4d54bdd2fe4bb2
Katerina <katerinaldg@gmail.com>
2018-01-30 21:20:53 +0000

    Sorts a list of three numbers

--- src/main/sorter/Sorter.java
+++ src/main/sorter/Sorter.java
@@ -4,16 +4,21 @@ import java.util.List;
 
 public class Sorter {
     public List<Integer> sortList(List<Integer> unsortedList) {
+        boolean swapped = true;
 
-        for (int i = 0; i < unsortedList.size() - 1; i++) {
+        while (swapped == true) {
 
-            if (unsortedList.get(i) > unsortedList.get(i + 1)) {
+            swapped = false;
+            for (int i = 0; i < unsortedList.size() - 1; i++) {
+                if (unsortedList.get(i) > unsortedList.get(i + 1)) {
 
-                int currentValue = unsortedList.get(i);
-                unsortedList.set(i, unsortedList.get(i + 1));
-                unsortedList.set(i + 1, currentValue);
-            } else {
-                return unsortedList;
+                    int currentValue = unsortedList.get(i);
+                    unsortedList.set(i, unsortedList.get(i + 1));
+                    unsortedList.set(i + 1, currentValue);
+                    swapped = true;
+                } else {
+                    return unsortedList;
+                }
             }
         }
         return unsortedList;
